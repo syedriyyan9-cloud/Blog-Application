cbcf430e7d0654ce0585570ec2a97d91dcfe87d1
Syed Riyyan Hassan <riyyanhassan789@gmail.com>
2026-02-22 02:11:00 +0500

    Completed post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.conf import settings
+
 from django.utils import timezone
 
 class Post(models.Model):
@@ -16,6 +18,7 @@ class Post(models.Model):
     create = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts")
 
     class Meta:
         """Defines metadata of models"""
